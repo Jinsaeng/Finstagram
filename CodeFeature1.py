d7b69c8612d02349e4f134187d989b55a5f69748
Jinsaeng <39491892+Jinsaeng@users.noreply.github.com>
2019-11-25 01:44:22 -0500

    Add files via upload
    Feature1, find posted photos that a user is following

---
+++ CodeFeature1.py
@@ -0,0 +1,12 @@
+#View visible photos:: Finstagram shows the user the photoID and photoPoster of each
+#photo that is visible to her, arranged in reverse chronological order.
+
+@app.route('/show_photos')
+def show_photos();
+    user = ['username']
+    cursor = conn.cursor()
+    query = 'SEELCT ID, username FROM Photo JOIN Person USING (username) WHERE AllFollowers IN %s AND followStatus = TRUE ORDER BY DESC'
+    cursor.execute(query, user)
+    photos = cursor.fetchall()
+    cursor.close()
+    return render_template('photos.html', username = user, posts = photos)
